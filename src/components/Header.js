c2243f074ab6bb7dd01ea61d1d5e3fe5fcb9fe5d
i67ahmed <i67ahmed@gmail.com>
2021-08-23 18:30:30 -0400

    dark mode toggle added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
+
+    function myFunction() {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+    }
+
     return (
      <div>
-       
+       <img src= "..\assets\hospiq.logo.png" alt="Hospital IQ Logo"></img>
+      <button onClick = {myFunction}>
+        Toggle to Dark Mode
+      </button>
      </div>
     )
   }
